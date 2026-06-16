848c08285b67904949dba01bfbc49625d53a476b
hasanniloy122-rgb <hasanniloy122@gmail.com>
2026-06-16 12:17:55 +0600

    added pet data to mongodb using post method

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require("cors")
 dotenv.config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.MONGODB_URI;
 
 const app = express();
 const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -20,14 +24,25 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    
     await client.connect();
-    // Send a ping to confirm a successful connection
+
+    const db = client.db("petnest")
+
+    const petCollection = db.collection("pets");
+
+    app.post('/pet', async (req, res) => {
+        const petData = req.body
+        console.log(petData);
+        const result = await petCollection.insertOne(petData)
+        res.json(result);
+    })
+    
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    
+    // await client.close();
   }
 }
 run().catch(console.dir);
